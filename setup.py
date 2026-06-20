af3b2d623d6b52b0afbbe4acc0aaaccdf9efb3f2
aneriai <aneri300625@gmail.com>
2026-06-20 15:20:20 +0530

    sturcture done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aneri Bhavsar",
+    author_email="aneribhavsar300625@gmail.com",
+    packages=find_packages()
+)
